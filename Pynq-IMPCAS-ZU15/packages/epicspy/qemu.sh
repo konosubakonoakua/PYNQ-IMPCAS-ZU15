13173957e78cf88d93b2c8fbc20e19e771ba3bf6
rafael.ren <rafael.ren@foxmail.com>
2026-04-17 12:16:37 +0800

    feat(pkg): epicspy add more libs

--- Pynq-IMPCAS-ZU15/packages/epicspy/qemu.sh
+++ Pynq-IMPCAS-ZU15/packages/epicspy/qemu.sh
@@ -19,15 +19,22 @@ log "[epicspy] start"
 
 # chown -R xilinx:xilinx /opt/epics
 
-# python3 -m pip install numpy==1.21.5
-python3 -m pip install pyserial smbus3 spidev
-python3 -m pip install caproto
+python3 -m pip install numpy==1.21.5
+python3 -m pip install pyserial==3.5 smbus3==0.5.5 spidev==3.8
+python3 -m pip install caproto==1.3.0
 python3 -m pip install grpcio==1.64.0 grpcio-tools==1.64.0
+python3 -m pip install fastapi==0.136.0 uvicorn==0.44.0 python-socketio==5.16.1
+python3 -m pip install Jinja2==3.0.3
+
+python3 -m pip install phoebusgen==3.2.0
+python3 -m pip install phoebusgen==3.2.0
 python3 -m pip install epicsdbbuilder==1.5
 python3 -m pip install epicscorelibs==7.0.10.99.0.0
 python3 -m pip install pvxslibs==1.5.1 --no-deps
 python3 -m pip install pyepics==3.5.2 --no-deps
 python3 -m pip install cothread==2.18.3 --no-deps
 python3 -m pip install softioc==4.6.1 --no-deps
+
+python3 -c "import softioc;print(softioc.__version__)"
 
 log "[epicspy] done"
